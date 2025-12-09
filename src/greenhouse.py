9d4da2cb8c86881799a3ef5fed71bb077f766a9f
mariocosenza <61911701+mariocosenza@users.noreply.github.com>
2025-12-09 14:28:38 +0100

    Green phase US2 test_turn_off_sprinkler

--- src/greenhouse.py
+++ src/greenhouse.py
@@ -37,8 +37,8 @@ class Greenhouse:
         self.sprinkler_on = True
 
     def turn_off_sprinkler(self) -> None:
-        # To be implemented
-        pass
+        GPIO.output(self.SPRINKLER_PIN, GPIO.LOW)
+        self.sprinkler_on = False
 
     def manage_sprinkler(self) -> None:
         # To be implemented
